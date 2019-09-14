c88cf5e029ed505b1f45edf521fff56e0f1aebb2
Valentine A <my30sat@gmail.com>
2019-09-14 12:07:56 +0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 $to = 'dgrgg@ukr.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Вы получили уведомление с вашего сайта Cleanko.cf.\n\n"."Данные сообщения:\n\nИмя: $name\n\nEmail: $email_address\n\nТекст сообщения:\n$message";
-$headers = "From: noreply@cleanko.cf\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@cleanko.ml\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
